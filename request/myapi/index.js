aa84b051efa9a98397a9aa1834b92aedff8cd51d
sk <290548180@qq.com>
2020-01-05 19:41:33 +0800

    fetch用法

--- request/myapi/index.js
+++ request/myapi/index.js
@@ -11,9 +11,8 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
-  res.header('Access-Control-Allow-Headers', 'Content-Type')
-  res.header('Access-Control-Allow-Headers', 'mytoken')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, mytoken')
+  res.header('Content-Type', 'application/json')
   next()
 })
 app.get('/async1', (req, res) => {
