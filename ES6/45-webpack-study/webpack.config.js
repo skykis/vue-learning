8eac7b8fa1ffa623b0abda0eba763c9210319425
sk <290548180@qq.com>
2020-01-11 23:48:08 +0800

    less/sass loader以及extractText插件去重问题

--- ES6/45-webpack-study/webpack.config.js
+++ ES6/45-webpack-study/webpack.config.js
@@ -1,11 +1,13 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 // htmlPlugin插件配置
 const htmlPlugin = new HtmlWebpackPlugin({
   template: path.join(__dirname, './src/index.html'),
   filename: 'index.html'
 })
+const extractText = new ExtractTextPlugin('style.css')
 
 module.exports = {
   mode: 'development', // 构建模式（development 或 production）
@@ -14,8 +16,27 @@ module.exports = {
     path: path.join(__dirname, './dist'),
     filename: 'bundle.js' // 输入文件的名称,默认main.js
   }, // 输出目录，默认值为 ./dist
-  plugins: [htmlPlugin],
+  plugins: [htmlPlugin, extractText],
   module: {
-    rules: [{ test: /\.css$/, use: ['style-loader', 'css-loader'] }]
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })
+      },
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'less-loader']
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
   }
 }
