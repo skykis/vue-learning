7daa7b804635a29be362ff7cf6d1efddbc7cdb7f
sk <290548180@qq.com>
2020-01-11 20:38:34 +0800

    webpack 入口和出口配置

--- ES6/45-webpack-study/webpack.config.js
+++ ES6/45-webpack-study/webpack.config.js
@@ -1,3 +1,10 @@
+const path = require('path')
+
 module.exports = {
-  mode: 'development' // 构建模式（development 或 production）
+  mode: 'development', // 构建模式（development 或 production）
+  entry: path.join(__dirname, './src/index.js'), // 构建依赖的入口，默认值为./src
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: 'bundle.js' // 输入文件的名称,默认main.js
+  } // 输出目录，默认值为 ./dist
 }
