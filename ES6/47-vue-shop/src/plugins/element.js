abe8f9ec8300558fc1a4dfc5828f5b96b52d45ee
sk <290548180@qq.com>
2020-01-13 00:52:03 +0800

    登录组件页面布局

--- ES6/47-vue-shop/src/plugins/element.js
+++ ES6/47-vue-shop/src/plugins/element.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import { Button, Row } from 'element-ui'
+import { Form, FormItem } from 'element-ui'
+import { Input } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Row)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
