21dc7cd61837e4bc2ec9449d03153aad704afa5c
WBX <290548180@qq.com>
2020-01-13 17:57:47 +0800

    登录页面完善 (表单校验 axios调接口 loading）

--- ES6/47-vue-shop/src/components/Login.vue
+++ ES6/47-vue-shop/src/components/Login.vue
@@ -6,15 +6,32 @@
         <img src="../assets/logo.png" />
       </div>
       <!-- 表单区域 -->
-      <el-form class="login_form">
+      <el-form
+        ref="loginFormRef"
+        status-icon
+        class="login_form"
+        :model="loginForm"
+        :rules="loginFormRules"
+      >
         <!-- 用户名 -->
-        <el-form-item><el-input></el-input></el-form-item>
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="el-icon-s-custom"
+            v-model="loginForm.username"
+          ></el-input>
+        </el-form-item>
         <!-- 密码 -->
-        <el-form-item><el-input></el-input></el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            type="password"
+            prefix-icon="el-icon-key"
+            v-model="loginForm.password"
+          ></el-input>
+        </el-form-item>
         <!-- 按钮 -->
         <el-form-item class="btns">
-          <el-button type="primary">登录</el-button>
-          <el-button type="info">重置</el-button>
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -22,7 +39,55 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 登录数据绑定对象
+      loginForm: {
+        username: '',
+        password: ''
+      },
+      // 表单验证
+      loginFormRules: {
+        // 验证密码
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' },
+          { min: 3, max: 10, message: '长度在 3 到 10 个字符', trigger: 'blur' }
+        ],
+        // 验证密码
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' },
+          { min: 6, max: 15, message: '长度在 6 到 15 个字符', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    // 重置
+    resetLoginForm() {
+      // ref="loginFormRef"。重置表单引用对象，去除validate和恢复date初始化状态
+      this.$refs.loginFormRef.resetFields()
+    },
+    login() {
+      // 表单引用对象验证表单
+      this.$refs.loginFormRef.validate(async valid => {
+        if (valid) {
+          // async/await axios调后台接口得到返回解构后的data
+          const { data: res } = await this.$axios.post('login', this.loginForm)
+          console.log(res)
+          if (res.meta.status !== 200) {
+            return this.$message.error('登录失败：' + res.meta.msg)
+          } else {
+            this.$message.success('登录成功')
+          }
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped>
