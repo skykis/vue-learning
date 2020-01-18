1884f50bfd8f77d5750af035d4377b13be0e6106
sk <290548180@qq.com>
2020-01-18 17:56:26 +0800

    用户列表基本查询功能实现

--- ES6/47-vue-shop/src/components/user/Users.vue
+++ ES6/47-vue-shop/src/components/user/Users.vue
@@ -5,20 +5,106 @@
       <!-- 搜索与添加区域 -->
       <el-row :gutter="20">
         <el-col :span="12">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query">
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="getUserList"
+            />
           </el-input>
         </el-col>
         <el-col :span="6">
           <el-button type="primary" icon="el-icon-search">添加用户</el-button>
         </el-col>
       </el-row>
+
+      <!-- 用户列表区域 -->
+      <el-table :data="userList" stripe border>
+        <el-table-column type="index" />
+        <el-table-column label="姓名" prop="username" />
+        <el-table-column label="邮箱" prop="email" />
+        <el-table-column label="电话" prop="mobile" />
+        <el-table-column label="角色" prop="role_name" />
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state" disabled />
+          </template>
+        </el-table-column>
+        <el-table-column label="操作" width="180px">
+          <template slot-scope="">
+            <!-- 修改 -->
+            <el-button type="primary" icon="el-icon-edit" size="mini" />
+            <!-- 删除 -->
+            <el-button type="danger" icon="el-icon-delete" size="mini" />
+            <!-- 分配角色 -->
+            <el-tooltip
+              class="item"
+              effect="dark"
+              content="分配角色"
+              placement="top"
+              :enterable="false"
+            >
+              <el-button type="warning" icon="el-icon-setting" size="mini" />
+            </el-tooltip>
+          </template>
+        </el-table-column>
+      </el-table>
+      <!-- 分页 -->
+      <el-pagination
+        @size-change="sizeChange"
+        @current-change="currentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      />
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      // 定义获取用户列表的参数
+      queryInfo: {
+        query: '',
+        pagenum: 1, // 当前页码
+        pagesize: 2 // 当前页显示数量
+      },
+      userList: [],
+      total: 0
+    }
+  },
+  created() {
+    this.getUserList()
+  },
+  methods: {
+    async getUserList() {
+      const { data: res } = await this.$axios.get('users', {
+        params: this.queryInfo
+      })
+      console.log(res)
+      if (res.meta.status !== 200) {
+        return this.$message.error('获取用户列表失败')
+      } else {
+        this.userList = res.data.users
+        this.total = res.data.total
+      }
+    },
+    // 监听pagesize变化事件
+    sizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getUserList()
+    },
+    // 监听pagenum变化事件
+    currentChange(newNum) {
+      this.queryInfo.pagenum = newNum
+      this.getUserList()
+    }
+  }
+}
 </script>
 
 <style lang="less" scoped></style>
--- ES6/47-vue-shop/src/plugins/element.js
+++ ES6/47-vue-shop/src/plugins/element.js
@@ -16,7 +16,12 @@ import {
   Breadcrumb,
   BreadcrumbItem,
   Card,
-  Col
+  Col,
+  Table,
+  TableColumn,
+  Switch,
+  Tooltip,
+  Pagination
 } from 'element-ui'
 
 Vue.use(Button)
@@ -35,4 +40,9 @@ Vue.use(Breadcrumb)
 Vue.use(BreadcrumbItem)
 Vue.use(Card)
 Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
 Vue.prototype.$message = Message
