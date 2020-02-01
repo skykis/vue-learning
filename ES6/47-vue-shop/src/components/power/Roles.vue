5bd9b5072bf1b06dba6b3b4c663837dede7bb601
sk <290548180@qq.com>
2020-02-01 22:48:09 +0800

    删除角色权限功能实现

--- ES6/47-vue-shop/src/components/power/Roles.vue
+++ ES6/47-vue-shop/src/components/power/Roles.vue
@@ -12,6 +12,7 @@
         <!-- 展开列 -->
         <el-table-column type="expand">
           <template slot-scope="scope">
+            <!-- <pre>{{ scope.row }}</pre> -->
             <el-row
               :class="['bdBottom', i1 === 0 ? 'bdTop' : '', 'vCenter']"
               v-for="(item1, i1) in scope.row.children"
@@ -19,7 +20,9 @@
             >
               <!-- 一级权限 -->
               <el-col :span="5">
-                <el-tag>{{ item1.authName }}</el-tag>
+                <el-tag closable @close="removeRightById(scope.row, item1.id)">
+                  {{ item1.authName }}
+                </el-tag>
                 <i class="el-icon-caret-right" />
               </el-col>
               <!-- 二三级权限 -->
@@ -30,7 +33,13 @@
                   :key="item2.id"
                 >
                   <el-col :span="6">
-                    <el-tag type="success">{{ item2.authName }}</el-tag>
+                    <el-tag
+                      type="success"
+                      closable
+                      @close="removeRightById(scope.row, item2.id)"
+                    >
+                      {{ item2.authName }}
+                    </el-tag>
                     <i class="el-icon-caret-right" />
                   </el-col>
                   <el-col :span="18">
@@ -38,6 +47,8 @@
                       type="warning"
                       v-for="item3 in item2.children"
                       :key="item3.id"
+                      closable
+                      @close="removeRightById(scope.row, item3.id)"
                     >
                       {{ item3.authName }}
                     </el-tag>
@@ -265,6 +276,32 @@ export default {
           return false
         }
       })
+    },
+    // 根据id删除权限
+    async removeRightById(role, rightId) {
+      // 弹框提示用户是否需要删除
+      const confirmResult = await this.$confirm(
+        '此操作将永久删除该权限, 是否继续?',
+        '提示',
+        {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }
+      ).catch(err => err)
+      if (confirmResult !== 'confirm') {
+        return this.$message.info('取消删除')
+      } else {
+        const { data: res } = await this.$axios.delete(
+          `roles/${role.id}/rights/${rightId}`
+        )
+        if (res.meta.status !== 200) {
+          return this.$message.error('删除权限失败：' + res.meta.msg)
+        } else {
+          role.children = res.data
+          this.$message.success('删除权限成功')
+        }
+      }
     }
   }
 }
