30725f717addffa872d66d2eaeb62859f1c44a30
sk <290548180@qq.com>
2020-05-14 00:17:42 +0800

    vuex

---
+++ vuex/01-vuex/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <h2>--------app内容--------</h2>
+    <h2>{{ $store.state.counter }}</h2>
+    <button @click="$store.state.counter++">++</button>
+    <button @click="$store.state.counter--">--</button>
+    <h2>--------hello vuex内容--------</h2>
+    <hello-vuex></hello-vuex>
+  </div>
+</template>
+
+<script>
+import HelloVuex from './components/HelloVuex'
+
+export default {
+  name: 'App',
+  components: { HelloVuex },
+  data() {
+    return {
+      message: 'app组件'
+    }
+  }
+}
+</script>
+
+<style></style>
