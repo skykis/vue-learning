db1b9aa85634a4ae6e9af23144ce4d9e88e7ce46
sk <290548180@qq.com>
2020-06-29 22:50:38 +0800

    vuex

--- vuex/01-vuex/src/App.vue
+++ vuex/01-vuex/src/App.vue
@@ -2,8 +2,15 @@
   <div id="app">
     <h2>--------app内容--------</h2>
     <h2>{{ $store.state.counter }}</h2>
+    <h2>{{ $store.getters.showNum }}</h2>
     <button @click="$store.state.counter++">++</button>
     <button @click="$store.state.counter--">--</button>
+    <button @click="handle1">++</button>
+    <button @click="handle2">--</button>
+    <button @click="handle3(3)">+3</button>
+    <button @click="handleAsync1">Async++</button>
+    <button @click="handleAsync2">Async--</button>
+    <button @click="handleAsync3(3)">Async+3</button>
     <h2>--------hello vuex内容--------</h2>
     <hello-vuex></hello-vuex>
   </div>
@@ -18,6 +25,28 @@ export default {
   data() {
     return {
       message: 'app组件'
+    }
+  },
+  methods: {
+    // 第一种mutations调用
+    handle1() {
+      this.$store.commit('plus')
+    },
+    handle2() {
+      this.$store.commit('minus')
+    },
+    handle3(n) {
+      this.$store.commit('plusN', n)
+    },
+    // 第一种Actions调用
+    handleAsync1() {
+      this.$store.dispatch('plusAsync')
+    },
+    handleAsync2() {
+      this.$store.dispatch('minusAsync')
+    },
+    handleAsync3(n) {
+      this.$store.dispatch('plusNAsync', n)
     }
   }
 }
--- vuex/01-vuex/src/store/index.js
+++ vuex/01-vuex/src/store/index.js
@@ -9,9 +9,42 @@ const store = new Vuex.Store({
   state: {
     counter: 1000
   },
-  mutations: {},
-  actions: {},
-  getters: {},
+  // 不要在mutations中写异步操作
+  mutations: {
+    plus(state) {
+      state.counter++
+    },
+    minus(state) {
+      state.counter--
+    },
+    plusN(state, n) {
+      state.counter += n
+    }
+  },
+  // 用于异步操作
+  actions: {
+    plusAsync(context) {
+      setTimeout(() => {
+        context.commit('plus')
+      }, 1000)
+    },
+    minusAsync(context) {
+      setTimeout(() => {
+        context.commit('minus')
+      }, 1000)
+    },
+    plusNAsync(context, n) {
+      setTimeout(() => {
+        context.commit('plusN', n)
+      }, 1000)
+    }
+  },
+  // 类似计算属性
+  getters: {
+    showNum: state => {
+      return 'now counter:' + state.counter
+    }
+  },
   modules: {}
 })
 
